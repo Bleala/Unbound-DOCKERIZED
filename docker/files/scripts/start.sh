b32b2718299593223719f9534bc64317c6ff07c4
Bleala <51357182+Bleala@users.noreply.github.com>
2025-05-27 23:04:50 +0200

    perf: root key file check/creation

--- docker/files/scripts/start.sh
+++ docker/files/scripts/start.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-# Create trust anchor file on start
-/opt/unbound/sbin/unbound-anchor -a "/etc/unbound/root.key"
+# Check if the root key file exists
+if [ ! -f "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}" ]
+then
+    # Create trust anchor file on start
+    /opt/unbound/sbin/unbound-anchor -a "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}"
+
+    # Set permissions for the directory
+    chmod 644 "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}"
+    chown "${UNBOUND_USER:-unbound}":"${UNBOUND_USER:-unbound}" "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}"
+fi
 
 # Check if an unbound configuration file is provided
 if [ ! -f "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}" ]
@@ -10,9 +18,12 @@ then
     cp /files/examples/unbound.conf "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}"
 
     # Set permissions for the configuration file
-    chmod 600 "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}"
+    chmod 644 "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}"
     chown "${UNBOUND_USER:-unbound}":"${UNBOUND_USER:-unbound}" "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}"
 fi
+
+# Switch to the unbound directory
+cd /var/unbound || exit 1
 
 # Start unbound
 exec /opt/unbound/sbin/unbound -d -c "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}"
