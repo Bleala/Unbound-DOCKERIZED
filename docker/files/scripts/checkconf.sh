c21baf2815aa1da065ae0f2f2c49707b4f420d89
Bleala <51357182+Bleala@users.noreply.github.com>
2025-05-28 08:47:39 +0200

    perf: unbound config check

--- docker/files/scripts/checkconf.sh
+++ docker/files/scripts/checkconf.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
-# Create trust anchor file on start
-/opt/unbound/sbin/unbound-anchor -a "/etc/unbound/root.key"
+# Check if the root key file exists
+if [ ! -f "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}" ]
+then
+    # Create trust anchor file on start
+    /opt/unbound/sbin/unbound-anchor -a "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}"
 
-# Check if an unbound configuration file is valid
-/opt/unbound/sbin/unbound-checkconf
+    # Set permissions for the root.key file
+    chmod 644 "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}"
+    chown "${UNBOUND_USER:-unbound}":"${UNBOUND_USER:-unbound}" "${UNBOUND_ROOT_FILE:-/var/unbound/root.key}"
+fi
+
+# Check if an unbound configuration file is provided
+if [ -f "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}" ]
+then
+    # Check if an unbound configuration file is valid
+    /opt/unbound/sbin/unbound-checkconf "${UNBOUND_CONFIG:-/etc/unbound/unbound.conf}"
+
+# What to do, if no unbound configuration file is provided
+else
+    # Warn if no configuration file is provided
+    echo "No unbound configuration file provided!"
+    echo "Exiting with error code 1."
+
+    # Exit with error code 1
+    exit 1
+fi
